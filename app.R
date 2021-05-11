46102904693939f0239da18587688d457f678585
vjsp <vj.sanchezp@gmail.com>
2021-05-11 03:04:57 +0200

    Con interacción entre gráficos

--- app.R
+++ app.R
@@ -1,4 +1,8 @@
 # Install and load necessary libraries
+if(!require(dplyr)) install.packages("dplyr",
+                                    repos = "http://cran.us.r-project.org")
+if(!require(fmsb)) install.packages("fmsb",
+                                    repos = "http://cran.us.r-project.org")
 if(!require(geojsonio)) install.packages("geojsonio",
                                          repos = "http://cran.us.r-project.org")
 if(!require(leaflet)) install.packages("leaflet",
@@ -20,7 +24,6 @@ gei <- read_excel("data/GEI.xlsx")
 colnames(countries)[1] <- "Country"
 my_data <- merge(gei, countries, by="Country")
 current_year <- max(my_data$Year)
-
 my_pal <- colorNumeric(palette="viridis",
                        domain=my_data$`Overall Gender Equality Index`,
                        na.color="transparent")
@@ -42,7 +45,8 @@ basemap <- leaflet(plot_map) %>%
   addLegend("bottomright",
             pal = my_pal,
             values = ~cv_large_countries$`Overall Gender Equality Index`,
-            title = "<small>GEI</small>")
+            title = "<small>GEI</small>") %>%
+  setView(8.44, 56.00, 3)
 
 ui <- bootstrapPage(
   navbarPage("Gender App", id="nav",
@@ -51,12 +55,13 @@ ui <- bootstrapPage(
                         sidebarPanel(
                           sliderTextInput("plot_date",
                                           label = h5("Select year"),
-                                          choices = unique(my_data$Year),
+                                          choices = sort(unique(my_data$Year)),
                                           selected = current_year,
                                           grid = FALSE,
                                           animate = animationOptions(interval = 3000,
                                                                      loop = FALSE)
-                          )
+                          ),
+                          plotOutput('radarPlot')
                         ),
                         mainPanel(leafletOutput("mymap"))
                       )
@@ -65,6 +70,23 @@ ui <- bootstrapPage(
 )
 
 server <- function(input, output, session) {
+  
+  reative_rc = reactive({
+    req(input$mymap_shape_click)
+    
+    radarchart_data <- as.data.frame(reactive_db()) %>%
+      filter(alpha.3 == country_selected()) %>%
+      select("Country", ends_with("(Domain score)")) %>%
+      rename_with(~ gsub(' (Domain score)', '', .x, fixed=TRUE))
+    rownames(radarchart_data) <- radarchart_data[,1]
+    radarchart_data[,1] <- NULL
+    radarchart_data <- rbind(rep(100,6) , rep(0,6) , radarchart_data)
+    radarchart_data
+    })
+  
+  country_selected <- reactive({
+    input$mymap_shape_click$id
+  })
   
   formatted_date = reactive({
     input$plot_date
@@ -85,6 +107,10 @@ server <- function(input, output, session) {
     large_countries
   })
   
+  reactive_chart_color = reactive({
+    reactive_db_large() %>% filter(alpha.3 == country_selected())
+  })
+  
   output$mymap <- renderLeaflet({basemap})
   
   observeEvent(input$plot_date, {
@@ -98,6 +124,7 @@ server <- function(input, output, session) {
                   smoothFactor = 0.1,
                   fillOpacity = 0.15,
                   fillColor = ~my_pal(reactive_db_large()$`Overall Gender Equality Index`),
+                  layerId = reactive_polygons()@data$adm0_a3,
                   label = paste(
                     "Country: ", reactive_db_large()$Country,"<br/>", 
                     "Work: ", reactive_db_large()$`Work (Domain score)`, "<br/>", 
@@ -110,6 +137,28 @@ server <- function(input, output, session) {
                     direction = "auto"
                   ))
   })
+  
+  observeEvent(input$mymap_shape_click,{
+    click<-input$mymap_shape_click
+    print(click$id)
+    print(click$lng)
+    print(click$lat)
+    leafletProxy("mymap") %>%
+      addPopups(click$lng, click$lat, click$id)
+    
+  })
+  
+  output$radarPlot <- renderPlot({
+    radarchart(reative_rc(),
+               # Customize the polygon
+               pcol = my_pal(reactive_chart_color()$`Overall Gender Equality Index`),
+               pfcol = scales::alpha(my_pal(reactive_chart_color()$`Overall Gender Equality Index`),
+                                     0.5), plwd = 2, plty = 1,
+               # Customize the grid
+               cglcol = "grey", cglty = 1, cglwd = 0.8,
+               # Customize the axis
+               axislabcol = "grey"
+  )})
 }
 
 # Run the application 
